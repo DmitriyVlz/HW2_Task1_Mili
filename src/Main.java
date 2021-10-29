4ccbb767b8cfdcf802c80dc69e428378f6661abf
DmitriyVlz <appattit@mail.com>
2021-10-29 18:36:22 +0300

    ferst commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 2_000;
+        int bonus = 20;
+        int mili = amount / bonus;
+        System.out.println(mili);
+    }
+
+
+}
